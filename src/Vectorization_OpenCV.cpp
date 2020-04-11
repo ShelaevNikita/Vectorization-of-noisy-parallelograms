a316260084dd33700faee144568f2d51fcbb1220
DiscreetIntellectual <terentev-daniil@yandex.ru>
2020-04-11 14:40:22 +0300

    Added .txt parser and manual bunching of 1.txt

--- src/Vectorization_OpenCV.cpp
+++ src/Vectorization_OpenCV.cpp
@@ -6,6 +6,9 @@
 
 #include <opencv2/highgui.hpp>
 
+#include <fstream>
+#include <cmath>
+
 using namespace cv;
 using namespace std;
 
@@ -17,13 +20,19 @@ public:
         Mat lines;
         vector<Point2f> pointVector;
 
+        double rhoApproximation = 0.0;
+        double distance = 0.0;
+
         for (int i = 0; i < inputPoints->size(); i++)
         {
             pointVector.push_back(Point2f(inputPoints->at(i).first, inputPoints->at(i).second));
+
+            distance = hypot(inputPoints->at(i).first, inputPoints->at(i).second);
+            rhoApproximation = rhoApproximation < distance ? distance : rhoApproximation;
         }
 
-        double rhoMin = 0.0, rhoMax = 200.0, rhoStep = 0.1;
-        double thetaMin = 0.0, thetaMax = CV_PI * 2.0, thetaStep = CV_PI / 1800.0; //step ~~ 0.1 degree
+        double rhoMin = 0.0, rhoMax = rhoApproximation * 1.2, rhoStep = 0.1; // let's add 20% to rhoApproximation
+        double thetaMin = 0.0, thetaMax = CV_PI * 2.0, thetaStep = CV_PI / 7200.0; //step ~~ 0.1 degree (now 0.025 degrees)
         HoughLinesPointSet(pointVector, lines, 400, 1,
             rhoMin, rhoMax, rhoStep,
             thetaMin, thetaMax, thetaStep);
@@ -31,39 +40,102 @@ public:
         lines.copyTo(*outputLines);
     }
 
+
+    static void parsePoints(string path, vector<pair<float, float>>* points) {
+        ifstream inputFile(path);
+        pair<float, float> temp = make_pair(0.0, 0.0);
+
+        if (!inputFile.is_open()) {
+            throw ios_base::failure("Incorrect filepath");
+        }
+        else {
+            while (inputFile >> temp.first >> temp.second) {
+                points->push_back(temp);
+            }
+        }
+
+        inputFile.close();
+    }
+
+};
+
+class Bunch {
+
+public:
+    static void bunching(vector<Vec3d>* houghOutput, vector<vector<pair<double, double>>>* bunches) {
+        double k, b;
+
+        for (int i = 0; i < houghOutput->size(); i++) {
+            k = -cos(houghOutput->at(i).val[2]) / sin(houghOutput->at(i).val[2]); // val[1] == rho, val[2] == theta
+            b = houghOutput->at(i).val[1] / sin(houghOutput->at(i).val[2]);
+
+            if (houghOutput->at(i).val[1] >= 60.0 && houghOutput->at(i).val[1] <= 71.0 &&
+                houghOutput->at(i).val[2] >= 1.89 && houghOutput->at(i).val[2] <= 2.1) {
+
+                bunches->at(0).push_back(make_pair(k, b));
+            }
+            else if (houghOutput->at(i).val[1] >= 27.0 && houghOutput->at(i).val[1] <= 36.5 &&
+                houghOutput->at(i).val[2] >= 5.0 && houghOutput->at(i).val[2] <= 5.2) {
+
+                bunches->at(1).push_back(make_pair(k, b));
+            }
+            else if (houghOutput->at(i).val[1] >= 26.0 && houghOutput->at(i).val[1] <= 41.0 &&
+                houghOutput->at(i).val[2] >= 0.3 && houghOutput->at(i).val[2] <= 0.415) {
+
+                bunches->at(2).push_back(make_pair(k, b));
+            }
+            else if (houghOutput->at(i).val[1] >= 55.0 && houghOutput->at(i).val[1] <= 72.0 &&
+                houghOutput->at(i).val[2] >= 3.45 && houghOutput->at(i).val[2] <= 3.56) {
+
+                bunches->at(3).push_back(make_pair(k, b));
+            }
+        }
+    }
 };
 
 int main()
 {
+    
+    vector<pair<float, float>> points1, points2, points3;
+    Vectorization::parsePoints(".\\Materials\\1.txt", &points1);
+    Vectorization::parsePoints(".\\Materials\\2.txt", &points2);
+    Vectorization::parsePoints(".\\Materials\\3.txt", &points3);
 
-    // Set of 27 points from 1.txt
-    vector<pair<float, float>> points = {
-    { 8.04411f, 78.9279f }, { 8.89507f, 79.151f }, { 9.75144f, 79.5972f }, { 11.0208f, 79.4857f },
-    { 11.4095f, 78.0355f }, { 11.8096f, 77.1432f }, { 12.2081f, 76.2508f }, { 12.4008f, 75.5815f },
-    { 11.5546f, 75.3584f }, { 10.2843f, 74.9122f }, { 9.43571f, 74.466f }, { 8.1698f, 74.0198f },
-    { 7.32486f, 73.5736f }, { 6.48152f, 73.1274f }, { 5.22202f, 72.6812f }, { 4.38229f, 72.235f },
-    { 2.71349f, 72.3466f }, { 2.301f, 73.3505f }, { 1.88804f, 74.6891f }, { 1.47164f, 75.693f },
-    { 1.68387f, 76.2508f }, { 2.94818f, 76.4739f }, { 3.79413f, 76.9201 }, { 4.64168f, 77.3663f },
-    { 5.91321f, 77.8125f }, { 6.76436f, 78.2586f }, { 8.0422f, 78.8164f }
-    };
+
+    cout << points1.size() << " " << points2.size() << " " << points3.size() << endl; //Debug only!
+
 
     // MAIN CALL of parallelogram
-    vector<Vec3d> line3d; //vector of generated lines in (votes, rho, theta) form
-    Vectorization::parallelogram(&points, &line3d);
+    vector<Vec3d> line3dFirst, line3dSecond, line3dThird; //vector of generated lines in (votes, rho, theta) form
+    Vectorization::parallelogram(&points1, &line3dFirst);
+    Vectorization::parallelogram(&points2, &line3dSecond);
+    Vectorization::parallelogram(&points3, &line3dThird);
 
     // basic console output to rank all the lines by their votes
-    for (int i = 0; i < line3d.size(); i++) {
-        printf("votes:%d, rho:%.7f, theta:%.7f\n", (int)line3d.at(i).val[0], line3d.at(i).val[1], line3d.at(i).val[2]);
+    for (int i = 0; i < line3dFirst.size(); i++) {
+        printf("votes:%d, rho:%.7f, theta:%.7f\n", (int)line3dFirst.at(i).val[0], line3dFirst.at(i).val[1], line3dFirst.at(i).val[2]);
     }
+    printf("Number of lines: %d \n\n", line3dFirst.size());
 
-    // Plotting the lines
-    char hough_window[] = "1.txt through Hough";
+    for (int i = 0; i < line3dSecond.size(); i++) {
+        printf("votes:%d, rho:%.7f, theta:%.7f\n", (int)line3dSecond.at(i).val[0], line3dSecond.at(i).val[1], line3dSecond.at(i).val[2]);
+    }
+    printf("Number of lines: %d \n\n", line3dSecond.size());
 
-    Mat hough_image = Mat::zeros(700, 700, CV_8UC3);  // basic 700x700 image
+    for (int i = 0; i < line3dThird.size(); i++) {
+        printf("votes:%d, rho:%.7f, theta:%.7f\n", (int)line3dThird.at(i).val[0], line3dThird.at(i).val[1], line3dThird.at(i).val[2]);
+    }
+    printf("Number of lines: %d \n\n", line3dThird.size());
 
-    for (size_t i = 0; i < line3d.size(); i++)
+
+    // Plotting 1.txt set of lines
+    char hough_window1[] = "1.txt through Hough";
+
+    Mat hough_image1 = Mat::zeros(700, 700, CV_8UC3);  // basic 700x700 image
+
+    for (size_t i = 0; i < line3dFirst.size(); i++)
     {
-        double rho = line3d.at(i).val[1], theta = line3d.at(i).val[2];
+        double rho = line3dFirst.at(i).val[1], theta = line3dFirst.at(i).val[2];
         Point pt1, pt2;
         double a = cos(theta), b = sin(theta);
         double x0 = a * rho, y0 = b * rho;
@@ -71,15 +143,90 @@ int main()
         pt1.y = cvRound(y0 + 300 * (a) + 300);
         pt2.x = cvRound(x0 - 300 * (-b) + 300);
         pt2.y = cvRound(y0 - 300 * (a) + 300);
-        line(hough_image, pt1, pt2, Scalar(0, 0, 40 * ((int)line3d.at(i).val[0] - 1)), 1, LINE_AA);
+        line(hough_image1, pt1, pt2, Scalar(0, 0, 40 * ((int)line3dFirst.at(i).val[0] - 1)), 1, LINE_AA);
         
     }
 
-    printf("Number of lines: %d \n", line3d.size());
+    imshow(hough_window1, hough_image1);
+    moveWindow(hough_window1, 0, 10);
+    //waitKey(0);
 
-    imshow(hough_window, hough_image);
-    moveWindow(hough_window, 0, 200);
-    waitKey(0);
+
+    // Plotting 2.txt set of lines
+    char hough_window2[] = "2.txt through Hough";
+
+    Mat hough_image2 = Mat::zeros(700, 700, CV_8UC3);  // basic 700x700 image
+
+    for (size_t i = 0; i < line3dSecond.size(); i++)
+    {
+        double rho = line3dSecond.at(i).val[1], theta = line3dSecond.at(i).val[2];
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a * rho, y0 = b * rho;
+        pt1.x = cvRound(x0 + 300 * (-b) + 300); // сдвигаем точки на 300 пикселей для центрирования наших пучков
+        pt1.y = cvRound(y0 + 300 * (a) + 300);
+        pt2.x = cvRound(x0 - 300 * (-b) + 300);
+        pt2.y = cvRound(y0 - 300 * (a) + 300);
+        line(hough_image2, pt1, pt2, Scalar(0, 0, 40 * ((int)line3dSecond.at(i).val[0] - 1)), 1, LINE_AA);
+
+    }
+
+    imshow(hough_window2, hough_image2);
+    moveWindow(hough_window2, 200, 60);
+    //waitKey(0);
+
+
+    // Plotting 3.txt set of lines
+    char hough_window3[] = "3.txt through Hough";
+
+    Mat hough_image3 = Mat::zeros(700, 700, CV_8UC3);  // basic 700x700 image
+
+    for (size_t i = 0; i < line3dThird.size(); i++)
+    {
+        double rho = line3dThird.at(i).val[1], theta = line3dThird.at(i).val[2];
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a * rho, y0 = b * rho;
+        pt1.x = cvRound(x0 + 300 * (-b) + 300); // сдвигаем точки на 300 пикселей для центрирования наших пучков
+        pt1.y = cvRound(y0 + 300 * (a)+300);
+        pt2.x = cvRound(x0 - 300 * (-b) + 300);
+        pt2.y = cvRound(y0 - 300 * (a)+300);
+        line(hough_image3, pt1, pt2, Scalar(0, 0, 40 * ((int)line3dThird.at(i).val[0] - 1)), 1, LINE_AA);
+
+    }
+
+    imshow(hough_window3, hough_image3);
+    moveWindow(hough_window3, 400, 110);
+    //waitKey(0);   // calling waitkey(0) only after all the console output!!! next waitkey is below
+
+
+    // Bunching all together
+    vector<vector<pair<double, double>>> bunches(4);
+
+    Bunch::bunching(&line3dFirst, &bunches);
+    
+    for (int i = 0; i < bunches[0].size(); i++) {
+        cout << bunches[0][i].first << " " << bunches[0][i].second << endl;
+    }
+    printf("First bunch contains %d lines \n\n", bunches[0].size());
+
+    for (int i = 0; i < bunches[1].size(); i++) {
+        cout << bunches[1][i].first << " " << bunches[1][i].second << endl;
+    }
+    printf("Second bunch contains %d lines \n\n", bunches[1].size());
+
+    for (int i = 0; i < bunches[2].size(); i++) {
+        cout << bunches[2][i].first << " " << bunches[2][i].second << endl;
+    }
+    printf("Third bunch contains %d lines \n\n", bunches[2].size());
+
+    for (int i = 0; i < bunches[3].size(); i++) {
+        cout << bunches[3][i].first << " " << bunches[3][i].second << endl;
+    }
+    printf("Fourth bunch contains %d lines \n\n", bunches[3].size());
+
+
+    waitKey(0); // only after all the console output
 
     return 0;
 }
