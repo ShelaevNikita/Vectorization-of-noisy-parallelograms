4b09e541c5c94c5b498285f33ca436c79e49005f
maratdin7 <maratdin7@gmail.com>
2020-05-27 19:35:15 +0300

    new cmake

---
+++ include/debug.h
@@ -0,0 +1,21 @@
+//
+// Created by maratdin7 on 26.04.2020.
+//
+
+#ifndef VECORIZATION_PARALLELOGRAMS_DEBUG_H
+#define VECORIZATION_PARALLELOGRAMS_DEBUG_H
+
+#define DEBUG
+
+#ifdef DEBUG
+
+#define SAY(...) fprintf(stderr, __VA_ARGS__)
+
+#else
+
+#define SAY(X...)
+
+#endif
+
+
+#endif //VECORIZATION_PARALLELOGRAMS_DEBUG_H
---
+++ include/structures.h
@@ -0,0 +1,8 @@
+#ifndef STRUCTURES
+#define STRUCTURES
+
+struct lineABC {
+	double a, b, c;
+};
+
+#endif // !STRUCTURES
